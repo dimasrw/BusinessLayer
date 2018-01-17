7f9a1df8eca8aba7d2d637a50c077b0a8253d42c
Gosu Dev <luksza.piotr@gmail.com>
2018-01-17 11:43:41 +0100

    add helpers

---
+++ src/BusinessLayer.Model/Repository/DataOperationResult.T.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BusinessLayer.Model.Repository
+{
+    public class DataOperationResult<T> : BusinessOperationResult
+    {
+        private DataOperationResult(bool isOk, T value = default(T), string message = null, Exception exception = null)
+            : base(isOk, message, exception)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public static DataOperationResult<T> Ok(T value = default(T), string message = null)
+        {
+            return new DataOperationResult<T>(true, value, message);
+        }
+
+        public static DataOperationResult<T> Warning(T value = default(T), string message = null)
+        {
+            return new DataOperationResult<T>(false, value, message);
+        }
+
+        public static DataOperationResult<T> Error(Exception exception, T value = default(T), string message = null)
+        {
+            return new DataOperationResult<T>(false, value, message, exception);
+        }
+        public static DataOperationResult<T> Error(T value = default(T), string message = null)
+        {
+            return new DataOperationResult<T>(false, value, message, null);
+        }
+    }
+}
